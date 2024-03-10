a4ef874d7a1e6164af97c849be695e485e376e68
DaniCalderon98 <162929859+DaniCalderon98@users.noreply.github.com>
2024-03-10 18:32:01 -0500

    sistema

---
+++ Sistema_votacion/pagina_votacion.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/pagina_votacion.css">
+</head>
+<body>
+    <div class="padre">
+        <header class="encabezado">
+            <h1>Vota por tu favorito</h1>
+        </header>
+    </div>
+    
+        <section class="section">
+            <article class="candidato"></article>
+            <article class="candidato"></article>
+            <article class="candidato"></article>
+            <article class="candidato"></article>
+            <article class="candidato"></article>
+            <article class="candidato"></article>  
+            
+        </section>
+
+        <footer class="footer">
+            <div class="abajo">
+                <div class="BotonEnviar">
+                    <button class="enviar">Enviar</button>
+                </div>
+                <div class="VerResultados">
+                    <button class="resultados">Ver Resultados</button>
+                </div>
+            </div>
+        </footer>
+   
+</body>
+</html>
